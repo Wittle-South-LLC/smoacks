e2606eaedfa7c007f1bb9e542be66c62005abd3b
Eric Wittle <eric@wittle.net>
2019-05-14 06:34:31 -0400

    With version change so it can be pushed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='smoacks',
-      version='0.1.6',
+      version='0.1.7',
       description='Simple Microservices with OpenAPI, Connexion, Kubernetes, and SQLAlchemy',
       url='https://github.com/wittlesouth/smoacks',
       author='Wittle South Ventures, LLC',
