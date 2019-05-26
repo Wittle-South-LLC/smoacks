cd8406a4d02f4f7b402b2e2eee5bd83c1bbe97fb
Eric Wittle <eric@wittle.net>
2019-05-26 17:35:17 -0400

    #11 - Read smoacks.yaml from conf if it exists, apply overrides

--- smoacks/sconfig.py
+++ smoacks/sconfig.py
@@ -10,21 +10,31 @@ env_defaults_overrides = ['SMOACKS_APP_NAME']
 
 LOGGER = logging.getLogger('SMOACKS')
 
-with open('conf/smoacks_default.yaml', 'r') as yamlconfig:
-    sconfig = yaml.load(yamlconfig, Loader=yaml.FullLoader)
+sconfig = None
+custom_config = None
 
-    for ovr in structure_overrides:
-        if ovr in os.environ:
-            sconfig['structure'][ovr[8:].lower()] = os.environ[ovr]
-    for ovr in parameters_overrides:
-        if ovr in os.environ:
-            sconfig['parameters'][ovr[8:].lower()] = os.environ[ovr]
-    for ovr in env_defaults_overrides:
-        if ovr in os.environ:
-            sconfig['env_defaults'][ovr.lower()] = os.environ[ovr]
-    sconfig['env_defaults']['smoacks_app_name_c'] = sconfig['env_defaults']['smoacks_app_name'].upper()
-    sconfig['env_defaults']['smoacks_app_name_k'] = sconfig['env_defaults']['smoacks_app_name'].replace('_', '-')
-    # Promote spec name from parameters into env defaults so it can more easily
-    # be used in jinja templates during generation
-    sconfig['env_defaults']['dest_spec'] = sconfig['parameters']['dest_spec']
+with open('conf/smoacks_default.yaml', 'r') as yaml_def_config:
+    sconfig = yaml.load(yaml_def_config, Loader=yaml.FullLoader)
+
+if os.path.isfile('conf/smoacks.yaml'):
+    with open('conf/smoacks.yaml', 'r') as yaml_config:
+        custom_config = yaml.load(yaml_config, Loader=yaml.FullLoader)
+        sconfig.update(custom_config)
+
+sconfig['env_defaults']['smoacks_app_name_c'] = sconfig['env_defaults']['smoacks_app_name'].upper()
+sconfig['env_defaults']['smoacks_app_name_k'] = sconfig['env_defaults']['smoacks_app_name'].replace('_', '-')
+# Promote spec name from parameters into env defaults so it can more easily
+# be used in jinja templates during generation
+sconfig['env_defaults']['dest_spec'] = sconfig['parameters']['dest_spec']
+
+for ovr in structure_overrides:
+    if ovr in os.environ:
+        sconfig['structure'][ovr[8:].lower()] = os.environ[ovr]
+for ovr in parameters_overrides:
+    if ovr in os.environ:
+        sconfig['parameters'][ovr[8:].lower()] = os.environ[ovr]
+for ovr in env_defaults_overrides:
+    if ovr in os.environ:
+        sconfig['env_defaults'][ovr.lower()] = os.environ[ovr]
+
 LOGGER.debug('SMOACKS configuration: {}'.format(str(sconfig)))
