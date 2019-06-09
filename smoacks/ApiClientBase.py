1bed25c7729f8201e307f4eb5d8848acdd2cd608
Eric Wittle <eric@wittle.net>
2019-06-09 10:11:23 -0400

    #24 - ApiClientBase handles multi-key children

--- smoacks/ApiClientBase.py
+++ smoacks/ApiClientBase.py
@@ -4,16 +4,16 @@ from smoacks.api_util import call_api
 
 class ApiClientBase(ABC):
     _api_path = None
-    _id_field = None
+    _id_fields = None
 
     @abstractmethod
-    def get_id(self):
+    def get_ids(self):
         pass
 
-    def toJSON(self, deep=False):
+    def toJSON(self, deep=False, parent_id=None):
         result = {}
         for key, value in vars(self).items():
-            if not key.startswith('_'):
+            if not key.startswith('_') and not key == parent_id:
                 result[key] = value
         return result
 
@@ -22,12 +22,17 @@ class ApiClientBase(ABC):
                         '/{}'.format(self._api_path),
                         self.toJSON(True))
         if resp.status_code == 201:
-            return True, resp.json()[self._id_field]
+            # If id_fields is not list, we get a new ID back from create
+            # otherwise, we don't need a value from the response
+            if isinstance(self._id_fields, list):
+                return True, None
+            else:
+                return True, resp.json()[self._id_fields]
         return False, resp
 
     def save_update(self, session):
         resp = call_api(session, 'PUT',
-                        '/{}/{}'.format(self._api_path, self.get_id()),
+                        '/{}/{}'.format(self._api_path, '/'.join(self.get_ids())),
                         self.toJSON(False))
         if resp.status_code == 201:
             return True, None
@@ -35,15 +40,19 @@ class ApiClientBase(ABC):
 
     def save_delete(self, session):
         resp = call_api(session, 'DELETE',
-                        '/{}/{}'.format(self._api_path, self.get_id()))
+                        '/{}/{}'.format(self._api_path, '/'.join(self.get_ids())))
         if resp.status_code == 204:
             return True, None
         return False, resp
 
     @classmethod
-    def get(cls, session, id):
+    def get(cls, session, ids):
+        # Second argument can be a string if child class has only one ID field,
+        # should be a list otherwise. We'll convert to a list so that we can join
+        # it around the API path separator character regardless
+        ids_arg = [ids] if type(ids) == str else ids
         resp = call_api(session, 'GET',
-                        '/{}/{}'.format(cls._api_path, id))
+                        '/{}/{}'.format(cls._api_path, '/'.join(ids_arg)))
         if resp.status_code == 200:
             return True, cls(**resp.json())
         return False, resp
